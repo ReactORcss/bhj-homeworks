312e88ebe0d0dc85e2829eb3ef9d99435fe7e054
ReactORcss <57535924+ReactORcss@users.noreply.github.com>
2020-06-08 14:22:27 +0300

    Update task.js

---
+++ dom/reveal/task.js
@@ -0,0 +1,19 @@
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    const intViewportHeight = window.innerHeight;
+
+    window.addEventListener('scroll', () => {
+        for (let i = 0; i < reveals.length; i++) {
+            const top = reveals[i].getBoundingClientRect().top;
+            const bottom = reveals[i].getBoundingClientRect().bottom;
+            if (top <= intViewportHeight) {
+                reveals[i].classList.add('reveal_active');
+            }
+            if (bottom <= 0 || top >= intViewportHeight) {
+                reveals[i].classList.remove('reveal_active');
+            }
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', reveal);

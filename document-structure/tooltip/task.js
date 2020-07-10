945b5d8c0c630e2986951c28c8141e3f60ea4351
ReactORcss <57535924+ReactORcss@users.noreply.github.com>
2020-07-10 14:22:39 +0300

    Update task.js

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,21 @@
+function tooltip() {
+    const linksToTooltips = document.querySelectorAll('.has-tooltip');
+    for (const linkToTooltip of linksToTooltips) {
+        linkToTooltip.addEventListener('click', showTooltip);
+    };
+
+    function showTooltip(event) {
+        event.preventDefault();
+        const link = event.target;
+        if (link.querySelector('.tooltip')) {
+            link.querySelector('.tooltip').classList.toggle('tooltip_active');
+        } else {
+            const tooltip = document.createElement('div');
+            tooltip.textContent = link.getAttribute('title');
+            tooltip.classList.add('tooltip', 'tooltip_active');
+            link.insertAdjacentElement('beforeEnd', tooltip);
+        }
+    };
+};
+
+document.addEventListener('DOMContentLoaded', tooltip);
